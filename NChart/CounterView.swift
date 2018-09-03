18f4e939b68a9960059d6925937321d6ce8a4133
Ngoc LE <ngoc.le@smarp.com>
2018-09-03 13:38:34 +0300

    Draw marker line for counter view

--- NChart/CounterView.swift
+++ NChart/CounterView.swift
@@ -68,5 +68,36 @@ import UIKit
         outlineColor.setStroke()
         outlinePath.lineWidth = Constants.lineWidth
         outlinePath.stroke()
+        
+        //Counter View markers
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.saveGState()
+        outlineColor.setFill()
+        
+        let markerWidth: CGFloat = 5.0
+        let markerSize: CGFloat = 10.0
+        
+        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize))
+        
+        context.translateBy(x: rect.width / 2, y: rect.height / 2)
+        
+        for i in 1...Constants.numberOfGlasses {
+            //4 - save the centred context
+            context.saveGState()
+            //5 - calculate the rotation angle
+            let angle = arcLengthPerGlass * CGFloat(i) + startAngle - .pi / 2
+            //rotate and translate
+            context.rotate(by: angle)
+            context.translateBy(x: 0, y: rect.height / 2 - markerSize)
+            
+            //6 - fill the marker rectangle
+            markerPath.fill()
+            //7 - restore the centred context for the next rotate
+            context.restoreGState()
+        }
+        
+        //8 - restore the original state in case of more painting
+        context.restoreGState()
     }
 }
