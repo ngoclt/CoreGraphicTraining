7a5871107641d417655b3e0a62c14e56c8397a69
Ngoc LE <ngoc.le@smarp.com>
2018-09-03 14:21:25 +0300

    Added ChartViewController

---
+++ NChart/Example/ChartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChartViewController.swift
+//  NChart
+//
+//  Created by Ngoc LE on 9/3/18.
+//  Copyright © 2018 Ngoc LE. All rights reserved.
+//
+
+import UIKit
+
+class ChartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
